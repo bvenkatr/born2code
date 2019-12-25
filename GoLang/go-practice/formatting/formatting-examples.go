9bd7638d1d3151fe28abd5b175d972354ef713d0
venkat <bvenkatr2@gmail.com>
2019-12-25 12:22:13 +0530

    format the custom type value

--- GoLang/go-practice/formatting/formatting-examples.go
+++ GoLang/go-practice/formatting/formatting-examples.go
@@ -5,12 +5,22 @@ import (
 	ioutil "io/ioutil"
 )
 
+type point struct{ x, y int64 }
+
+func (p point) String() string {
+	return fmt.Sprintf("Point(%d, %d)", p.x, p.y)
+}
+
+func (p point) GoString() string {
+	return fmt.Sprintf("Point(%d, %d)", p.x, p.y)
+}
+
 var (
 	i = 20
 	f = 2.903
 	s = "Clear is better than clever"
 	b = true
-	p = struct{ x, y int64 }{256, 314}
+	p = point{256, 314}
 )
 
 func OutputToWriter() (string, error) {
@@ -28,8 +38,11 @@ var bi int64 = -83948723884
 var ui uint64 = 4590349530945
 
 func OutputToStdout() {
-	fmt.Println(i, f, s, b, p, bi, ui)
-	fmt.Printf("i = %v\nf = %v\ns = %s\nb = %v\np = %v\nbi = %v\nui = %v\n", i, f, s, b, p, bi, ui)
-	fmt.Println()
-	fmt.Printf("i = %#v\nf = %#v\ns = %#v\nb = %#v\np = %#v\nbi = %#v\nui = %#v\n", i, f, s, b, p, bi, ui)
+	// fmt.Println(i, f, s, b, p, bi, ui)
+	// fmt.Printf("i = %v\nf = %v\ns = %s\nb = %v\np = %v\nbi = %v\nui = %v\n", i, f, s, b, p, bi, ui)
+	// fmt.Println()
+	// fmt.Printf("i = %#v\nf = %#v\ns = %#v\nb = %#v\np = %#v\nbi = %#v\nui = %#v\n", i, f, s, b, p, bi, ui)
+
+	fmt.Println(p)
+	fmt.Printf("%#v\n", p)
 }
--- GoLang/go-practice/main.go
+++ GoLang/go-practice/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	test "./formatting"
-	"fmt"
 )
 
 func main() {
-	// test.OutputToStdout()
-	filename, err := test.OutputToWriter()
-	fmt.Println(filename, err)
+	test.OutputToStdout()
+	// filename, err := test.OutputToWriter()
+	// fmt.Println(filename, err)
 }
