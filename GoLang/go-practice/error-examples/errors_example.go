9f2d2fb947dcd0d2f28f20f2051cb8deb622a934
venkat <bvenkatr2@gmail.com>
2020-01-01 09:54:01 +0530

    handle errors with switch case

--- GoLang/go-practice/error-examples/errors_example.go
+++ GoLang/go-practice/error-examples/errors_example.go
@@ -1,19 +1,51 @@
 package error_examples
 
-import "log"
+import (
+	"errors"
+	"log"
+)
 
+
+var (
+	errBadThing = errors.New("something bad")
+	errWorseThing = errors.New("worse thing")
+)
 func ErrorExamples() {
 
-	defer func() {
-		if err := recover(); err != nil {
-			log.Printf("boom() failed %v\n", err)
-		}
-	}()
+	//////////////////////////////////////////////////
+	//defer func() {
+	//	if err := recover(); err != nil {
+	//		log.Printf("boom() failed %v\n", err)
+	//	}
+	//}()
+	//
+	//log.Println("hello gophers")
+	//boom()
+	/////////////////////////
 
-	log.Println("hello gophers")
-	boom()
+	/////////////////////////
+	err := doSomethingBad()
+	//err = doSomethingWorse()
+
+	if err != nil {
+		switch err {
+		case errBadThing:
+			log.Printf("Uh oh: %s\n", err)
+		case errWorseThing:
+			log.Printf("Abandon ship!\n")
+		}
+	}
+	/////////////////////////
 }
 
 func boom() {
 	panic("BOOM!")
+}
+
+func doSomethingBad() error {
+	return errBadThing
+}
+
+func doSomethingWorse() error {
+	return errWorseThing
 }
